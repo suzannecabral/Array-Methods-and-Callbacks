ebe3575c7d8dee3717519f5582f753a4fd6949de
Suzanne Cabral <suzannecabral@live.com>
2020-08-20 09:10:53 -0700

    Task 2 complete - get Finals

--- index.js
+++ index.js
@@ -42,17 +42,22 @@ console.log("----------- Task 2: getFinals -------------");
 
 function getFinals(data) {
 
-   /* code here */
-
+    const finalsData = data.filter((items)=>{
+        return items.Stage === "Final";
+    });
+    console.log(finalsData);
 };
 
+getFinals(fifaData);
 // console.log(getFinals(fifaData));
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
 
-    /* code here */
+function getYears() {
+
+    
 
 };
 
